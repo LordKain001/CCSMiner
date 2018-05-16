375b0b07f8d8e528ce889faeb851ad7fc3cae10d
LordKain001 <35432416+LordKain001@users.noreply.github.com>
2018-05-16 20:12:21 +0200

    updated Temperature output

--- MinerAlive/MinerAlive.php
+++ MinerAlive/MinerAlive.php
@@ -1,19 +1,144 @@
 
 <?php
+
+function getDataFromShell(&$Data, $processCommand, $dir)
+{
+	$descriptorspec = array(
+	   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+	   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+	   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+	);
+
+	$env = array('some_option' => 'aeiou');
+
+$process = array(
+	"process" => $processCommand,
+	"directory" => $dir,
+	"descriptorspec"  => $descriptorspec,
+	"pipes" => NULL,
+	"resource" => NULL,
+	);
+	
+$process["resource"] = proc_open($process["process"], $process["descriptorspec"], $process["pipes"], $process["directory"], $env );	
+//var_dump($xmrStakProcess);	
+
+sleep(5);
+
+if (is_resource($process["resource"])) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+	
+	$pipes = $process["pipes"];
+	
+	//stream_set_timeout($pipes[1], 2);
+	stream_set_blocking($pipes[1], FALSE);
+	$Data = stream_get_contents($pipes[1]);// read from the pipe 
+	 
+    unset($pipes);
+
+
+}else
+ {
+	throw new Exception('No Process Stream Ressource');
+ }
+
+}
+
+function isAmdPart($var)
+{
+	
+	if(substr( $var, 0, strlen($query) ) === $query)
+	{
+		return true;
+	}
+}
+
+function getGpuTemps($verboseLevel = NULL)
+{
+	$sensors = Null;
+	
+
+	try {
+		getDataFromShell($sensors,"sensors","");
+		
+	} catch (Exception $e) {
+		echo"Error Reading Sensor Data";
+	}
+
+ 	$sensors = explode("\n\n",$sensors);
+ 	$temp = [];
+ 	$query = "amd";
+ 	array_pop($sensors);
+ 
+
+ 	foreach ($sensors as &$value) {
+ 		$value =explode("\n",$value);
+
+ 		$value[2] = preg_replace('!\s+!', ' ',$value[2]);
+ 		$value[2] = explode(" ",$value[2]);
+		array_shift($value[2]);
+		$value[2] = array_shift($value[2]);
+ 		
+
+ 		$value[3] = preg_replace('!\s+!', ' ',$value[3]);
+ 		$value[3] = explode(" ",$value[3]);
+		array_shift($value[3]);
+		$value[3] = array_shift($value[3]);
+ 		
+
+ 		if (substr( $value[0], 0, strlen($query) ) === $query) {
+ 			$temp[$value[0]] = [];
+ 			$temp[$value[0]] += ["FanSpeed" => $value[2]];
+ 			$temp[$value[0]] += ["temp" => $value[3]];
+ 		}
+
+
+ 			
+ 		
+
+ 	}
+
+
+ 	
+ 	
+	$sensors = json_encode($temp);
+
+	if ($verboseLevel) {
+		echo "\n";
+		var_dump($sensors);
+	}
+
+	return $sensors;
+
+}
+
+
+
+
 $configFile = '../config.json';
 
 	//check for Reset
 	//If it cant pass the whole script the Error count will increase(2 times wirte on file)
-	$file = './Status.php';
-	try {
-		$status = json_decode(file_get_contents($file), TRUE);	
-	} catch (Exception $e) {
-		echo "Failed";
-	}
+	$statusFile = './status.json';
 
+
+	if (file_exists($statusFile))
+	{
+		$status = json_decode(file_get_contents($statusFile), TRUE);	
+	}else
+	{
+		$status = array(
+			"errors" => 0,
+			"sucess" => 0,
+		);
+
+		file_put_contents($statusFile, json_encode($status));
+	}
 	
-	echo "\nErrors:" . $status["errors"];
-	echo "\nsuccesses:" . $status["sucess"];
+	echo "\nErrors:" . $status["errors"] . "\n";
+	echo "successes:" . $status["sucess"]. "\n";
 
 	$status["errors"]++;
 	if($status["errors"] >= 10)
@@ -23,12 +148,12 @@ $configFile = '../config.json';
 		echo "------------------------------------------------------------------------------------------------\n";
 		$status["errors"] = 0;
 		$status["sucess"] = 0;
-		file_put_contents($file, json_encode($status));
+		file_put_contents($statusFile, json_encode($status));
 		shell_exec("echo s | sudo tee /proc/sysrq-trigger");
 		shell_exec("echo U | sudo tee /proc/sysrq-trigger");
 		shell_exec("echo b | sudo tee /proc/sysrq-trigger");
 	}
-	file_put_contents($file, json_encode($status));
+	file_put_contents($statusFile, json_encode($status));
 
 	//Get Data from PC
 	$output = shell_exec('ls -lart');
@@ -36,7 +161,7 @@ $configFile = '../config.json';
 	$ipAdress = shell_exec("/sbin/ifconfig | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'");
 	$mininglog = shell_exec('journalctl -u ccsMiner.service -n50');
 	$Timestamp = shell_exec('date');
-	$temperature = shell_exec('sensors');
+	$temperature = getGpuTemps(1);
 	$hostName = shell_exec('hostname');
 	$uptime = shell_exec("uptime -p");
 
@@ -104,7 +229,7 @@ $configFile = '../config.json';
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 
 	//Send echos to return var
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
 	 
 	//Set the content type to application/json
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
@@ -123,7 +248,7 @@ $configFile = '../config.json';
 	}
 	curl_close($ch);
 
-	file_put_contents($file, json_encode($status));
+	file_put_contents($statusFile, json_encode($status));
 
 exit;
 
